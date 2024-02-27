261e9140befe4d6e7afde73a6fe128cee5b1ab84
Chad A. Woitas <woitas.chad@gmail.com>
2024-02-27 12:48:46 -0600

    Update numberdisplay.py
    I did this in github editor... your mileage may vary

--- Designer/des/src/libdes/widgets/numberdisplay.py
+++ Designer/des/src/libdes/widgets/numberdisplay.py
@@ -1,12 +1,47 @@
 import os
 
 from PyQt5.QtWidgets import QApplication, QLabel
+# Need direct import, designer will occasionally throw errors if you use QtCore.pyqtProperty
+#   ... Still yet to figure out why...
+from PyQt5.QtCore import pyqtProperty
 
 class PyNumberDisplay(QLabel):
 	def __init__(self, parent=None):
 		super(PyNumberDisplay, self).__init__(parent)
+		self.sometogglething = False
+		self.somecountthing = 0
+		self.somefloatthing = 0.0
 
-		
+	def get_toggle(self):
+		return self.sometogglething
+
+	def set_toggle(self, data):
+		# Sometime Validation is good here
+		self.sometoggletthing = data
+
+	# Important this goes after the functions.
+	#   Why I have yet to figure out it shouldn't matter
+        #   I really dislike inline defines like this personally
+	sometogglething = pyqtProperty(int, get_toggle, set_toggle)
+	
+	def get_counter(self):
+		return self.somecountthing
+
+	def set_counter(self, data):
+		# Sometime Validation is good here
+		self.somecountthing = data
+
+	somecountthing = pyqtProperty(int, get_counter, set_counter)
+
+	def get_floater(self):
+		return self.somefloatthing
+
+	def set_floater(self, data):
+		# Sometime Validation is good here
+		self.somefloatthing = data
+
+	somefloatthing = pyqtProperty(int, get_floater, set_floater)
+
 
 if __name__ == "__main__":
 	import sys
