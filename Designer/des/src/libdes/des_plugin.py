ba4e4f673c02a495bad648c39d2be765ea923abe
John Thornton <dev@gnipsel.com>
2024-02-27 07:33:37 -0600

    add example of adding a widget to designer
    Signed-off-by: John Thornton <dev@gnipsel.com>

---
+++ Designer/des/src/libdes/des_plugin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Print Out to see designer loaded it
+print("Loading Des Plugins ============================")
+
+from libdes.python.analogclockplugin import PyAnalogClockPlugin
+#from libjet.widgets.plugins.halmeter_plugin import HalMeterWidgetPlugin
+#from libjet.widgets.plugins.test_plugin import TestWidgetPlugin
+
+print("Finished loading")
